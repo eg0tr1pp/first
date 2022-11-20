db099f3de0771a491f0ae51e39546960809b3d2c
eg0tr1pp <turin.artem2004@ya.ru>
2022-11-20 17:44:30 +0300

    квадратные уравнения

---
+++ 1ex.py
@@ -0,0 +1,18 @@
+a = input()
+b = input()
+c = input()
+if type(a) == str and type(b) == str and type(c) == str:
+    print('недопустимое значение')
+else:
+    x1 = 0
+    x2 = 0
+    D = b ** 2 - 4 * a * c
+    if D > 0:
+        x1 = (-b + D ** (1 / 2)) / (2 * a)
+        x2 = (-b - D ** (1 / 2)) / (2 * a)
+        print(x1, x2)
+    if D == 0:
+        x1 = (-b + D ** (1 / 2)) / (2 * a)
+        print(x1)
+    if D < 0:
+        print('корней уравнения не существует')
