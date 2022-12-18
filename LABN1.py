59e9d5c609cb84fb10c80ba4858da6eb3b9d3a05
eg0tr1pp <118464404+eg0tr1pp@users.noreply.github.com>
2022-12-18 21:54:20 +0300

    Update LABN1.py

--- LABN1.py
+++ LABN1.py
@@ -1,18 +1,13 @@
-a = input()
-b = input()
-c = input()
-if type(a) == str and type(b) == str and type(c) == str:
-    print('введено недопустимое значение')
-else:
-    x1 = 0
-    x2 = 0
-    D = b ** 2 - 4 * a * c
-    if D > 0:
-        x1 = (-b + D ** (1 / 2)) / (2 * a)
-        x2 = (-b - D ** (1 / 2)) / (2 * a)
-        print(x1, x2)
-    if D == 0:
-        x1 = (-b + D ** (1 / 2)) / (2 * a)
-        print(x1)
-    if D < 0:
-        print('корней уравнения нет')
+a = int(input())
+b = int(input())
+c = int(input())
+D = b ** 2 - 4 * a * c
+if D > 0:
+    x1 = (-b + D ** (1 / 2)) / (2 * a)
+    x2 = (-b - D ** (1 / 2)) / (2 * a)
+    print(x1, x2)
+if D == 0:
+    x1 = (-b + D ** (1 / 2)) / (2 * a)
+    print(x1)
+if D < 0:
+    print('корней уравнения нет')
